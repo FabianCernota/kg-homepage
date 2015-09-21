72d939a90ec334204ae8b48af08039119a926973
Fabian Cernota <fabian.cernota@chamaeleon.de>
2015-09-21 15:33:56 +0200

    Contact.php

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
               <form>
                 <h1 style="text-align:center;">Kontaktieren Sie uns!</h1>
                  <div class="form-group">
-                   <label for="name">Name:</label>
+                   <label for="name">Name: *</label>
                    <input type="text" class="form-control" id="name" placeholder="Name">
                  </div>
                  <div class="form-group">
-                   <label for="email">E-Mail:</label>
+                   <label for="email">E-Mail: *</label>
                    <input type="email" class="form-control" id="email" placeholder="E-Mail">
                  </div>
                  <div class="form-group">
@@ -25,7 +25,7 @@
                    <input type="tel" class="form-control" id="telefon" placeholder="Telefonnummer">
                  </div>
                  <div class="form-group">
-                   <label for="message">Nachricht:</label>
+                   <label for="message">Nachricht: *</label>
                    <textarea id="message" class="form-control" rows="5" placeholder="Ihre Nachricht"></textarea>
                  </div>
                  <div class="form-group">
